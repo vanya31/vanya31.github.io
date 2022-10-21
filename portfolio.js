929e6719a3e9ca87da2d48868eca6b2727ad3c26
Vanya Verma <72427243+vanya31@users.noreply.github.com>
2022-10-21 21:32:28 +0530

    update

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,14 @@
-// ????????????
-
-x = 3 + 4 + "7" ;
-console.log(x);
+AOS.init();
+window.onload = function() {
+    typeWriter();
+  };
+var i = 0;
+var txt = "Hi, I'm Vanya Verma, A Creative Front End Developer."; /* The text */
+var speed = 70; /* The speed/duration of the effect in milliseconds */
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
