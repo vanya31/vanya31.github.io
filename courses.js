b6f6a2922a9b1b7303569dfc52f5027f54c29c6c
Vanya Verma <72427243+vanya31@users.noreply.github.com>
2022-10-29 09:31:06 +0530

    update

---
+++ courses.js
@@ -0,0 +1,14 @@
+AOS.init();
+window.onload = function() {
+    typeWriter();
+  };
+var i = 0;
+var txt = "Courses"; /* The text */
+var speed = 70; /* The speed/duration of the effect in milliseconds */
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
---
+++ experience.js
@@ -0,0 +1,15 @@
+AOS.init();
+
+window.onload = function() {
+    typeWriter();
+  };
+var i = 0;
+var txt = "Experience"; /* The text */
+var speed = 70; /* The speed/duration of the effect in milliseconds */
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
